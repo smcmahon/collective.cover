b50980dfad695b14569223bbfef905d701a2b49a
Denis Krienbühl <denis@href.ch>
2013-12-02 10:14:31 +0100

    Supress unused template warnings for templates which are actually used.

--- src/collective/cover/__init__.py
+++ src/collective/cover/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import warnings
 
 from Products.CMFCore.permissions import setDefaultRoles
 from zope.i18nmessageid import MessageFactory
@@ -7,3 +8,24 @@ _ = MessageFactory('collective.cover')
 
 setDefaultRoles(
     'collective.cover: Can Export Layout', ('Manager', 'Site Administrator'))
+
+# supress unused template warnings from grok for templates we know are actually
+# used, but not stored on the template attribute of the class. Check layout.py
+# for an example of how many templates are used on a class, without being
+# associated with it.
+#
+# for more see https://github.com/collective/collective.cover/issues/362
+
+used_templates = [
+    'generalmarkup.pt',
+    'group.pt',
+    'row.pt',
+    'search_list.pt',
+    'tile.pt',
+    'tree_template.pt',
+]
+
+ignore_expr = (
+    r'Found.+unassociated template.+/({})'.format('|'.join(used_templates))
+)
+warnings.filterwarnings('ignore', category=UserWarning, message=ignore_expr)
